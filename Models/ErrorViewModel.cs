f572269b16b957234c48646cbacbb8eb066dca8e
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-12-13 11:44:52 +0500

    init proj

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Solovev.MVC.GameStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
